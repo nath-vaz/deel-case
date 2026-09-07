8ed944b1fef24b17f39c6823140a261c26b899f3
nath-vaz <nathaniel.vazelle@gmail.com>
2026-09-07 19:12:11 +0300

    Modify dataset to better fit business need

--- models/dataset/payments__acceptance_rate_over_time.sql
+++ models/dataset/payments__acceptance_rate_over_time.sql
@@ -5,28 +5,27 @@ with payments AS (
 select
     cast(date_trunc('day', created_at) as date) as aggregation_date,
     'daily' as aggregation_filter,
-    -- add dimensions based on analysts' needs
-    payment_country,
-    cast(count(case when is_accepted is true then 1 end) / count(*) as numeric(18, 2)) * 100 as acceptance_rate_pct
+    cast(count(case when is_accepted is true then 1 end) / count(*) as numeric(18, 2)) * 100 as acceptance_rate_pct,
+    cast(sum(case when is_accepted is true then payment_amount_in_dollars end) / sum(payment_amount_in_dollars) as numeric(18, 2)) * 100 as acceptance_rate_by_value_pct
 from payments
-group by 1, 2, 3
+group by 1, 2
 
 union
 
 select
     cast(date_trunc('week', created_at) as date) as aggregation_date,
     'weekly' as aggregation_filter,
-    payment_country,
-    cast(count(case when is_accepted is true then 1 end) / count(*) as numeric(18, 2)) * 100 as acceptance_rate_pct
+    cast(count(case when is_accepted is true then 1 end) / count(*) as numeric(18, 2)) * 100 as acceptance_rate_pct,
+    cast(sum(case when is_accepted is true then payment_amount_in_dollars end) / sum(payment_amount_in_dollars) as numeric(18, 2)) * 100 as acceptance_rate_by_value_pct
 from payments
-group by 1, 2, 3
+group by 1, 2
 
 union
 
 select
     cast(date_trunc('month', created_at) as date) as aggregation_date,
     'monthly' as aggregation_filter,
-    payment_country,
-    cast(count(case when is_accepted is true then 1 end) / count(*) as numeric(18, 2)) * 100 as acceptance_rate_pct
+    cast(count(case when is_accepted is true then 1 end) / count(*) as numeric(18, 2)) * 100 as acceptance_rate_pct,
+    cast(sum(case when is_accepted is true then payment_amount_in_dollars end) / sum(payment_amount_in_dollars) as numeric(18, 2)) * 100 as acceptance_rate_by_value_pct
 from payments
-group by 1, 2, 3
+group by 1, 2
